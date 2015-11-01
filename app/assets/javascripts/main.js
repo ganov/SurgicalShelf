9f956ae0a61cfe00e7e51900f00413c5466a6fe3
ganov <ganovelli@gmail.com>
2015-11-01 03:14:15 +0100

    Add controllers/Login.js and use it in main.js

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -20,8 +20,9 @@
         }
     });
 
-    require(["angular", "./services/playRoutes"], function() {
+    require(["angular", "./services/playRoutes", "./controllers/login"], function(a, b, login) {
         var app = angular.module("app", ["play.routing"]);
+        app.controller("LoginCtrl", login.LoginCtrl);
 
         angular.bootstrap(document, ["app"]);
     });
